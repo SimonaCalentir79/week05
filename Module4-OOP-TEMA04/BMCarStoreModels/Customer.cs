7b5384007e863f21d9b7c94704de4f8d8cd34813
SimonaCalentir79 <45466438+SimonaCalentir79@users.noreply.github.com>
2018-12-20 10:24:49 +0200

    Add files via upload
    homework

---
+++ Module4-OOP-TEMA04/BMCarStoreModels/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMCarStoreModels
+{
+    public class Customer:ICustomer
+    {
+        public string CustomerName { get; }
+        public string Email { get; }
+        public string TelephoneNumber { get; }
+
+        public Customer(string customerName, string email, string telephoneNumber)
+        {
+            this.CustomerName = customerName;
+            this.Email = email;
+            this.TelephoneNumber = telephoneNumber;
+        }
+
+        public void PrintCustomer()
+        {
+            Console.WriteLine($"\t Customer name: {this.CustomerName}, e-mail: {this.Email}, telephone: {this.TelephoneNumber}");
+        }
+    }
+}
---
+++ Module4-OOP-TEMA04/BMCarStoreModels/OrderStatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMCarStoreModels
+{
+    public class OrderStatus : IStatusOfOrder
+    {
+        public string StatusOfOrder { get; set; }
+        public DateTime DateOfStatus { get; set; }
+        public string Details { get; set; }
+
+        public OrderStatus(string statusOfOrder, DateTime dateOfStatus, string details)
+        {
+            this.StatusOfOrder = statusOfOrder;
+            this.DateOfStatus = dateOfStatus;
+            this.Details = details;
+        }
+
+        public void PrintStatus()
+        {
+            Console.WriteLine($"\t\t\t Status: {this.StatusOfOrder} - {this.DateOfStatus}");
+            Console.WriteLine($"\t\t\t Details: {this.Details}");
+        }
+    }
+}
